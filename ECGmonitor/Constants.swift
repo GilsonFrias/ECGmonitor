3353d87625eabf578a5f798634e9824f3cd8bfff
GilsonFrias <gfrias06@gmail.com>
2019-11-22 16:31:06 +0900

    first-commit

---
+++ ECGmonitor/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  ECGmonitor
+//
+//  Created by Gilson on 2019/11/20.
+//  Copyright © 2019 Gilosn Frias . All rights reserved.
+//
+
+import Foundation
---
+++ ECGmonitor/styledTxt.swift
@@ -0,0 +1,9 @@
+//
+//  styledTxt.swift
+//  ECGmonitor
+//
+//  Created by Gilson on 2019/11/22.
+//  Copyright © 2019 Gilosn Frias . All rights reserved.
+//
+
+import Foundation
